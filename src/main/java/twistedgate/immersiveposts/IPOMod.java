53007dbaa51386539a46ea6ef1389ebe3a7faead
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2019-05-01 03:16:37 +0200

    Update IPOMod.java
    Due to butterfingers i lost the keystore, so i had to make a new one.

--- src/main/java/twistedgate/immersiveposts/IPOMod.java
+++ src/main/java/twistedgate/immersiveposts/IPOMod.java
@@ -10,6 +10,6 @@ public class IPOMod{
 	public static final String DEPENDS		= "required-after:forge@[14.23.5.2768,);required-after:immersiveengineering@[0.12,)";
 	public static final String PROXY_CLIENT	= "twistedgate.immersiveposts.client.ClientProxy";
 	public static final String PROXY_SERVER	= "twistedgate.immersiveposts.common.CommonProxy";
-	public static final String CERT_PRINT	= "0ba25d8c0ec23537afc6db926e1ea764335a33b1";
+	public static final String CERT_PRINT	= "0ba8738eadcf158e7fe1452255a73a022fb15feb";
 	public static final String UPDATE_URL	= "https://raw.githubusercontent.com/TwistedGate/ImmersivePosts/master/change.json";
 }
