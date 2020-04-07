70c8f497002ce539daf40051bc59905e5e62545c
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2020-04-07 15:01:35 +0200

    Changed itemgroup creation
    using #12 as reference

--- src/main/java/twistedgate/immersiveposts/ImmersivePosts.java
+++ src/main/java/twistedgate/immersiveposts/ImmersivePosts.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -20,8 +21,14 @@ import twistedgate.immersiveposts.common.CommonProxy;
 @Mod(IPOMod.ID)
 public class ImmersivePosts{
 	
-	public static final ItemGroup creativeTab=new IPOItemGroup();
-	public static CommonProxy proxy=DistExecutor.runForDist(()->()->new ClientProxy(), ()->()->new CommonProxy());
+	public static final ItemGroup creativeTab=new ItemGroup("tab"+IPOMod.ID){
+		@Override
+		public ItemStack createIcon(){
+			return new ItemStack(IPOStuff.post_Base==null?Items.BARRIER:IPOStuff.post_Base);
+		}
+	};
+	
+	public static CommonProxy proxy=DistExecutor.runForDist(()->ClientProxy::new, ()->CommonProxy::new);
 	public static Logger log;
 	
 	public ImmersivePosts(){
@@ -48,27 +55,5 @@ public class ImmersivePosts{
 		System.err.println("\r\nTHIS IS NOT AN OFFICIAL BUILD OF "+IPOMod.NAME.toUpperCase()+"! Fingerprints: ["+event.getFingerprints()+"]");
 		System.err.println("If you didnt download this mod from the official curse page: Stop Drop and Roll this jar right out of your mods folder!\r\n");
 		// Guess thats what this would be used for? lol
-	}
-	
-	
-	public static class IPOItemGroup extends ItemGroup{
-		private ItemStack iconstack=null;
-		private IPOItemGroup(){
-			super(IPOMod.ID);
-		}
-		
-		@Override
-		public ItemStack createIcon(){
-			if(this.iconstack==null)
-				this.iconstack=new ItemStack(IPOStuff.post_Base);
-			return this.iconstack;
-		}
-		
-		@Override
-		public boolean equals(Object obj){
-			if(obj==this || obj instanceof IPOItemGroup) return true;
-			
-			return super.equals(obj);
-		}
 	}
 }
