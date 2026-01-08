e69d1db398131ea0f58ede219087c391fb2e4272
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2026-01-08 17:52:53 +0100

    Cherry-pick of 21fcb1113fbb96dd439578343bf316dfdf69e70e

--- src/main/java/twistedgate/immersiveposts/client/ColorHandler.java
+++ src/main/java/twistedgate/immersiveposts/client/ColorHandler.java
@@ -4,22 +4,18 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.color.block.BlockColor;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockAndTintGetter;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import twistedgate.immersiveposts.common.tileentity.PostBaseTileEntity;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class ColorHandler implements BlockColor{
 	@Override
-	public int getColor(@Nonnull BlockState state, BlockAndTintGetter world, BlockPos pos, int index){
-		if(index >= 0){
-			BlockEntity te = world.getBlockEntity(pos);
-			if(te instanceof PostBaseTileEntity base && !base.getStack().isEmpty()){
-				return Minecraft.getInstance().getBlockColors().getColor(base.getCoverState(), world, pos, index);
-			}
-		}
+	public int getColor(@Nonnull BlockState state, @Nullable BlockAndTintGetter world, @Nullable BlockPos pos, int index){
+		if(index < 0 || world == null || pos == null || !(world.getBlockEntity(pos) instanceof PostBaseTileEntity base) || base.getStack().isEmpty())
+			return 0xFFFFFF;
 		
-		return -1;
+		return Minecraft.getInstance().getBlockColors().getColor(base.getCoverState(), world, pos, index);
 	}
 }
